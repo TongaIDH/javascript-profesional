292f360fd5b75732331c3c84dc356d7109e6a16f
TongaIDH <gastonfga@hotmail.com>
2020-12-06 19:53:56 -0300

    mute/unmute function added

--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,11 @@
 import MediaPlayer from './MediaPlayer.js'
+import AutoPlay from './plugins/Autoplay.js'
 
 const video = document.querySelector("video");
-const player = new MediaPlayer({ el: video});
+const player = new MediaPlayer({ el: video, plugins: [new AutoPlay()] });
 
-const button = document.querySelector("button");
-button.onclick = () => player.play();
+const playButton = document.querySelector("button[id=playButton]");
+playButton.onclick = () => player.togglePlay();
+
+const soundButton = document.querySelector("button[id=soundButton]");
+soundButton.onclick = () => player.toggleSound();
