b58a7d08b15738ed4b3d857a89436a5060b5becd
TongaIDH <gastonfga@hotmail.com>
2020-12-07 13:52:50 -0300

    modified selectors within querySelector for playButton & soundButton

--- assets/index.js
+++ assets/index.js
@@ -2,10 +2,15 @@ import MediaPlayer from './MediaPlayer.js'
 import AutoPlay from './plugins/Autoplay.js'
 
 const video = document.querySelector("video");
-const player = new MediaPlayer({ el: video, plugins: [new AutoPlay()] });
+const player = new MediaPlayer({ 
+    el: video,
+    plugins: [
+        // new AutoPlay()
+    ],
+});
 
-const playButton = document.querySelector("button[id=playButton]");
+const playButton = document.querySelector("#playButton");
 playButton.onclick = () => player.togglePlay();
 
-const soundButton = document.querySelector("button[id=soundButton]");
+const soundButton = document.querySelector("#soundButton");
 soundButton.onclick = () => player.toggleSound();
